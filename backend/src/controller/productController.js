371aa067478d852a1b28fd931538adfbb247611a
Anusuya <anusuyasasi011gmail.com>
2026-03-15 19:10:00 +0530

    commit

--- backend/src/controller/productController.js
+++ backend/src/controller/productController.js
@@ -4,7 +4,7 @@ const asyncWrapper = require("../middleWare/asyncWrapper");
 const ApiFeatures = require("../utils/apiFeatures");
 const cloudinary = require("cloudinary");
 
-// >>>>>>>>>>>>>>>>>>>>> createProduct Admin route >>>>>>>>>>>>>>>>>>>>>>>>
+// >>>>>>>>>>>>>>>>>>>>> createProduct Admin route  >>>>>>>>>>>>>>>>>>>>>>>>
 exports.createProduct = asyncWrapper(async (req, res) => {
   let images = []; 
 
@@ -16,20 +16,25 @@ exports.createProduct = asyncWrapper(async (req, res) => {
     }
 
     const imagesLinks = [];
+
+    // Split images into chunks due to cloudinary upload limits only 3 images can be uploaded at a time so we are splitting into chunks and uploading them separately eg: 9 images will be split into 3 chunks and uploaded separately
     const chunkSize = 3;
     const imageChunks = [];
     while (images.length > 0) {
       imageChunks.push(images.splice(0, chunkSize));
     }
 
+
+    // Upload images in separate requests. for loop will run 3 times if there are 9 images to upload each time uploading 3 images at a time
     for (let chunk of imageChunks) {
       const uploadPromises = chunk.map((img) =>
         cloudinary.v2.uploader.upload(img, {
           folder: "Products",
         })
       );
+
       
-      const results = await Promise.all(uploadPromises);
+      const results = await Promise.all(uploadPromises); // wait for all the promises to resolve and store the results in results array eg: [{}, {}, {}] 3 images uploaded successfully and their details are stored in results array
 
       for (let result of results) { 
         imagesLinks.push({
@@ -44,47 +49,56 @@ exports.createProduct = asyncWrapper(async (req, res) => {
   }
 
   const data = await ProductModel.create(req.body);
+
   res.status(200).json({ success: true, data: data });
 });
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>> ALL PRODUCTS GET ROUTE (FIXED) >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> get all product >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 exports.getAllProducts = asyncWrapper(async (req, res) => {
-  const resultPerPage = 8;
-  const productsCount = await ProductModel.countDocuments();
+  const resultPerPage = 8; // Number of products visible per page
+  const productsCount = await ProductModel.countDocuments(); // Get total number of products
 
-  // 1. Initial Query setup with Search and Filter
+  // Create an instance of the ApiFeatures class, passing the ProductModel.find() query and req.query (queryString)
   const apiFeature = new ApiFeatures(ProductModel.find(), req.query)
-    .search()
-    .filter();
+    .search() // Apply search filter based on the query parameters
+    .filter(); // Apply additional filters based on the query parameters
 
-  // 2. Fetch all filtered products BEFORE pagination to get the correct count
-  let products = await apiFeature.query;
-  let filteredProductCount = products.length;
+  let products = await apiFeature.query; // Fetch the products based on the applied filters and search
 
-  // 3. Apply Pagination and use .clone() to avoid execution errors
-  apiFeature.Pagination(resultPerPage);
-  products = await apiFeature.query.clone();
+  let filteredProductCount = products.length; // Number of products after filtering (for pagination)
 
-  res.status(200).json({
+  apiFeature.Pagination(resultPerPage); // Apply pagination to the products
+
+  // Mongoose no longer allows executing the same query object twice, so use .clone() to retrieve the products again
+  products = await apiFeature.query.clone(); // Retrieve the paginated products
+
+  res.status(201).json({
     success: true,
-    products,
-    productsCount,
-    resultPerPage,
-    filteredProductCount,
+    products: products,
+    productsCount: productsCount,
+    resultPerPage: resultPerPage,
+    filteredProductCount: filteredProductCount,
   });
 });
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>> GET ALL PRODUCTS ADMIN >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> get all product admin route>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 exports.getAllProductsAdmin = asyncWrapper(async (req, res) => {
   const products = await ProductModel.find();
 
-  res.status(200).json({
+  res.status(201).json({  
     success: true,
     products,
   });
 });
 
-// >>>>>>>>>>>>>>>>>>>>>> Update Admin Route >>>>>>>>>>>>>>>>>>>>>>>
+  
+
+
+//>>>>>>>>>>>>>>>>>> Update Admin Route >>>>>>>>>>>>>>>>>>>>>>>
 exports.updateProduct = asyncWrapper(async (req, res, next) => {
   let product = await ProductModel.findById(req.params.id);
 
@@ -93,31 +107,32 @@ exports.updateProduct = asyncWrapper(async (req, res, next) => {
   }
 
   let images = [];
-  if (req.body.images) {
-    if (typeof req.body.images === "string") {
-      images.push(req.body.images);
-    } else {
-      images = req.body.images;
+
+  if (typeof req.body.images === "string") {
+    images.push(req.body.images);
+  } else {
+    images = req.body.images;
+  }
+
+  if (images !== undefined) {
+    // Deleting Images From Cloudinary
+    for (let i = 0; i < product.images.length; i++) {
+      await cloudinary.v2.uploader.destroy(product.images[i].product_id);
     }
 
-    if (images.length > 0) {
-      for (let i = 0; i < product.images.length; i++) {
-        await cloudinary.v2.uploader.destroy(product.images[i].product_id);
-      }
+    const imagesLinks = [];
+    for (let img of images) {
+      const result = await cloudinary.v2.uploader.upload(img, {
+        folder: "Products",
+      });
 
-      const imagesLinks = [];
-      for (let img of images) {
-        const result = await cloudinary.v2.uploader.upload(img, {
-          folder: "Products",
-        });
-
-        imagesLinks.push({
-          product_id: result.public_id,
-          url: result.secure_url,
-        });
-      }
-      req.body.images = imagesLinks;
+      imagesLinks.push({
+        product_id: result.public_id,
+        url: result.secure_url,
+      });
     }
+
+    req.body.images = imagesLinks;
   }
 
   product = await ProductModel.findByIdAndUpdate(req.params.id, req.body, {
@@ -126,90 +141,106 @@ exports.updateProduct = asyncWrapper(async (req, res, next) => {
     useFindAndModify: false,
   });
 
-  res.status(200).json({
+  res.status(201).json({
     success: true,
-    product,
+    product: product,
   });
 });
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>> DELETE PRODUCT >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  delete product --admin  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 exports.deleteProduct = asyncWrapper(async (req, res, next) => {
-  const product = await ProductModel.findById(req.params.id);
+  let product = await ProductModel.findById(req.params.id);
 
   if (!product) {
     return next(new ErrorHandler("Product not found", 404));
   }
 
+  // Deleting Images From Cloudinary
   for (let i = 0; i < product.images.length; i++) {
     await cloudinary.v2.uploader.destroy(product.images[i].product_id);
   }
 
   await product.remove();
 
-  res.status(200).json({
+  res.status(201).json({
     success: true,
-    message: "Product deleted successfully",
+    message: "Product delete successfully",
   });
 });
 
-// >>>>>>>>>>>>>>>>>>>>>>> Product Details >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//>>>>>>>>>>>>>>>>>>>>>>> Detils of product >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 exports.getProductDetails = asyncWrapper(async (req, res, next) => {
-  const product = await ProductModel.findById(req.params.id);
-  if (!product) {
+  const id = req.params.id;
+  const Product = await ProductModel.findById(id);
+  if (!Product) {
     return next(new ErrorHandler("Product not found", 404));
   }
-  res.status(200).json({
-    success: true,
-    product,
+  res.status(201).json({
+    succes: true,
+    Product: Product,
   });
 });
 
-// >>>>>>>>>> Create/Update Review >>>>>>>>>>
+//>>>>>>>>>>>>> Create New Review or Update the review >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 exports.createProductReview = asyncWrapper(async (req, res, next) => {
   const { ratings, comment, productId, title, recommend } = req.body;
   const review = {
     userId: req.user._id,
     name: req.user.name,
     ratings: Number(ratings),
-    title,
-    comment,
-    recommend,
-    avatar: req.user.avatar.url,
+    title: title,
+    comment: comment,
+    recommend: recommend,
+    avatar: req.user.avatar.url, // Add user avatar URL to the review object
   };
 
   const product = await ProductModel.findById(productId);
 
-  const isReviewed = product.reviews.find(
-    (rev) => rev.userId.toString() === req.user._id.toString()
-  );
+  // check if user already reviewed
+  const isReviewed = product.reviews.find((rev) => {
+    return rev.userId.toString() === req.user._id.toString();
+  });
 
   if (isReviewed) {
+    // Update the existing review
     product.reviews.forEach((rev) => {
       if (rev.userId.toString() === req.user._id.toString()) {
         rev.ratings = ratings;
         rev.comment = comment;
         rev.recommend = recommend;
+        
         rev.title = title;
+        product.numOfReviews = product.reviews.length;
       }
     });
   } else {
+    // Add a new review
     product.reviews.push(review);
     product.numOfReviews = product.reviews.length;
   }
 
-  let avg = 0;
+  // Calculate average ratings
+  let totalRatings = 0;
   product.reviews.forEach((rev) => {
-    avg += rev.ratings;
+    totalRatings += rev.ratings;
   });
-  product.ratings = avg / product.reviews.length;
+  product.ratings = totalRatings / product.reviews.length;
 
+  // Save to the database
   await product.save({ validateBeforeSave: false });
 
-  res.status(200).json({ success: true });
+  res.status(200).json({
+    success: true,
+  });
 });
 
-// >>>>>>>>>> Get All Reviews >>>>>>>>>>
+
+// >>>>>>>>>>>>>>>>>>>>>> Get All Reviews of a product>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 exports.getProductReviews = asyncWrapper(async (req, res, next) => {
+  // we need product id for all reviews of the product
+
   const product = await ProductModel.findById(req.query.id);
 
   if (!product) {
@@ -222,31 +253,54 @@ exports.getProductReviews = asyncWrapper(async (req, res, next) => {
   });
 });
 
-// >>>>>>>>>> Delete Review >>>>>>>>>>
+//>>>>>>>>>>>>>>>>>>>>>> delete review >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 exports.deleteReview = asyncWrapper(async (req, res, next) => {
+  // we have review id and product id here in req object
+  // find thr product with product id
+
   const product = await ProductModel.findById(req.query.productId);
 
   if (!product) {
     return next(new ErrorHandler("Product not found", 404)); 
   }
 
+  // check if ther any review avalible with given reviwe id. then filter the review array store inside reviews without that review
   const reviews = product.reviews.filter(
-    (rev) => rev._id.toString() !== req.query.id.toString()
+    (rev) => { return rev._id.toString() !== req.query.id.toString()}
   );
-
+  // once review filterd then update new rating from prdoduct review
   let avg = 0;
   reviews.forEach((rev) => {
+   
     avg += rev.ratings;
   });
 
-  const ratings = reviews.length === 0 ? 0 : avg / reviews.length;
-  const numOfReviews = reviews.length;
 
+  
+  let ratings = 0;
+  if (reviews.length === 0) {
+    ratings = 0;
+  } else {
+    ratings = avg / reviews.length;
+  }
+  // also set  numOfReviews in product
+  const numOfReviews = reviews.length;
+  // now update the product schema with these values
   await ProductModel.findByIdAndUpdate(
     req.query.productId,
-    { reviews, ratings, numOfReviews },
-    { new: true, runValidators: true, useFindAndModify: false }
+    {
+      reviews,
+      ratings,
+      numOfReviews,
+    },
+    {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    }
   );
 
-  res.status(200).json({ success: true });
+  res.status(200).json({
+    success: true,
+  });
 });
--- frontend/src/actions/productAction.jsx
+++ frontend/src/actions/productAction.jsx
@@ -21,185 +21,211 @@ import {
   UPDATE_PRODUCT_REQUEST,
   UPDATE_PRODUCT_SUCCESS,
   UPDATE_PRODUCT_FAIL,
-  ALL_REVIEW_REQUEST,
-  ALL_REVIEW_SUCCESS,
-  DELETE_REVIEW_REQUEST,
-  DELETE_REVIEW_SUCCESS,
-  DELETE_REVIEW_FAIL,
+ ALL_REVIEW_REQUEST,
+ ALL_REVIEW_SUCCESS,
+ DELETE_REVIEW_REQUEST,
+ DELETE_REVIEW_SUCCESS,
+ DELETE_REVIEW_FAIL,
   CLEAR_ERRORS,
   ALL_REVIEW_FAIL,
 } from "../constants/productsConstatns";
 
-// Get ALL Products
+// get ALL Products
 export const getProduct = (
   keyword = "",
   currentPage = 1,
-  price = [0, 250000], // Increased range to ensure all products are covered
+  price = [0, 100000],
   category,
   ratings = 0
-) => async (dispatch) => {
-  try {
-    dispatch({ type: ALL_PRODUCT_REQUEST });
+) => {
+  return async (dispatch) => {
+    try {
+      // initial state :
+      dispatch({
+        type: ALL_PRODUCT_REQUEST,
+      });
 
-    // Base URL
-    let link = `https://cricketstore.onrender.com/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}`;
+      let link = `https://cricketstore.onrender.com/api/v1/product?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}`;
 
-    // If category exists, add it to the URL
-    if (category) {
-      link = `https://cricketstore.onrender.com/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}&ratings[gte]=${ratings}`;
+      // when category selected by user then using another link
+      if (category) {
+        link = `https://cricketstore.onrender.com/api/v1/product?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}&category=${category}`;
+      }
+      const { data } = await axios.get(link);
+
+      dispatch({
+        type: ALL_PRODUCT_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: ALL_PRODUCT_FAIL,
+        payload: error.message,
+      });
     }
-
-    const { data } = await axios.get(link);
-
-    dispatch({
-      type: ALL_PRODUCT_SUCCESS,
-      payload: data,
-    });
-  } catch (error) {
-    dispatch({
-      type: ALL_PRODUCT_FAIL,
-      payload: error.response?.data?.message || error.message,
-    });
-  }
+  };
 };
 
-// Get Product Details
-export const getProductDetails = (id) => async (dispatch) => {
-  try {
-    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+// Get Products Details
+export const getProductDetails = (id) => {
+  return async (dispatch) => {
+    try {
+      dispatch({
+        type: PRODUCT_DETAILS_REQUEST,
+      });
 
-    const { data } = await axios.get(`https://cricketstore.onrender.com/api/v1/product/${id}`);
+      const { data } = await axios.get(`https://cricketstore.onrender.com/api/v1/product/${id}`);
 
-    dispatch({
-      type: PRODUCT_DETAILS_SUCCESS,
-      payload: data.product || data.Product, // Handling both naming cases
-    });
-  } catch (error) {
-    dispatch({
-      type: PRODUCT_DETAILS_FAIL,
-      payload: error.response?.data?.message || error.message,
-    });
-  }
+      dispatch({
+        type: PRODUCT_DETAILS_SUCCESS,
+        payload: data.Product,
+      });
+    } catch (error) {
+      dispatch({
+        type: PRODUCT_DETAILS_FAIL,
+        payload: error.message,
+      });
+    }
+  };
 };
 
-// Add new Review
+//Add new Review
 export const newReview = (reviewData) => async (dispatch) => {
   try {
     dispatch({ type: NEW_REVIEW_REQUEST });
 
-    const config = {
-      headers: { "Content-Type": "application/json" },
-      withCredentials: true,
-    };
+   const config = { 
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true // இங்கே சேர்க்கவும்
+};
 
     const { data } = await axios.put(`https://cricketstore.onrender.com/api/v1/review/new`, reviewData, config);
 
     dispatch({ type: NEW_REVIEW_SUCCESS, payload: data.success });
   } catch (error) {
-    dispatch({ type: NEW_REVIEW_FAIL, payload: error.response?.data?.message || error.message });
+    dispatch({ type: NEW_REVIEW_FAIL, payload: error.message });
   }
 };
 
-// Admin product request
+// admin product request :
 export const getAdminProducts = () => async (dispatch) => {
   try {
     dispatch({ type: ADMIN_PRODUCT_REQUEST });
 
-    const { data } = await axios.get("https://cricketstore.onrender.com/api/v1/admin/products", { withCredentials: true });
-    
+const { data } = await axios.get("https://cricketstore.onrender.com/api/v1/admin/products", { withCredentials: true });
     dispatch({ type: ADMIN_PRODUCT_SUCCESS, payload: data.products });
   } catch (error) {
-    dispatch({ type: ADMIN_PRODUCT_FAIL, payload: error.response?.data?.message || error.message });
+    dispatch({ type: ADMIN_PRODUCT_FAIL, payload: error.message });
   }
 };
 
 // Create Product
-export const createProduct = (productData) => async (dispatch) => {
-  try {
-    dispatch({ type: NEW_PRODUCT_REQUEST });
-
-    const config = {
-      headers: { "Content-Type": "application/json" }, // Usually JSON if not using direct FormData
-      withCredentials: true,
-    };
-
-    const { data } = await axios.post(
-      `https://cricketstore.onrender.com/api/v1/admin/product/new`,
-      productData,
-      config
-    );
-
-    dispatch({ type: NEW_PRODUCT_SUCCESS, payload: data });
-  } catch (error) {
-    dispatch({ type: NEW_PRODUCT_FAIL, payload: error.response?.data?.message || error.message });
-  }
+export function createProduct(productData) {
+  return async function(dispatch) {
+    try {
+      dispatch({
+        type: NEW_PRODUCT_REQUEST,
+      });
+         
+     const config = {
+  headers: { "Content-Type": "multipart/form-data" },
+  withCredentials: true // இங்கே சேர்க்கவும்
 };
 
-// Delete Product
-export const deleteProduct = (id) => async (dispatch) => {
-  try {
-    dispatch({ type: DELETE_PRODUCT_REQUEST });
+      const { data } = await axios.post(
+        `https://cricketstore.onrender.com/api/v1/admin/product/new`,
+        productData,
+        config
+      );
 
-    const { data } = await axios.delete(`https://cricketstore.onrender.com/api/v1/admin/product/${id}`, { withCredentials: true });
-    
-    dispatch({ type: DELETE_PRODUCT_SUCCESS, payload: data.success });
-  } catch (error) {
-    dispatch({ type: DELETE_PRODUCT_FAIL, payload: error.response?.data?.message || error.message });
-  }
-};
+      dispatch({
+        type: NEW_PRODUCT_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: NEW_PRODUCT_FAIL,
+        payload: error.message,
+      });
+    }
+  };
+}
 
-// Update Product
+// Delete Product request
+
+export function deleteProduct(id) {
+  return async function(dispatch) {
+    try {
+      dispatch({ type: DELETE_PRODUCT_REQUEST });
+
+const { data } = await axios.delete(`https://cricketstore.onrender.com/api/v1/admin/product/${id}`, { withCredentials: true });    
+      dispatch({ type: DELETE_PRODUCT_SUCCESS, payload: data.success });
+    } catch (error) {
+      dispatch({ type: DELETE_PRODUCT_FAIL, payload: error.message });
+    }
+  };
+}
+
+// updateProduct;
 export const updateProduct = (id, productData) => async (dispatch) => {
-  try {
-    dispatch({ type: UPDATE_PRODUCT_REQUEST });
+         try {
+           dispatch({ type: UPDATE_PRODUCT_REQUEST });
 
-    const config = {
-      headers: { "Content-Type": "application/json" },
-      withCredentials: true,
-    };
-
-    const { data } = await axios.put(
-      `https://cricketstore.onrender.com/api/v1/admin/product/${id}`,
-      productData,
-      config
-    );
-
-    dispatch({ type: UPDATE_PRODUCT_SUCCESS, payload: data.success });
-  } catch (error) {
-    dispatch({ type: UPDATE_PRODUCT_FAIL, payload: error.response?.data?.message || error.message });
-  }
+          const config = {
+  headers: { "Content-Type": "multipart/form-data" },
+  withCredentials: true // இங்கே சேர்க்கவும்
 };
 
-// Get all reviews (Admin)
-export const getAllReviews = (productId) => async (dispatch) => {
-  try {
-    dispatch({ type: ALL_REVIEW_REQUEST });
+           const { data } = await axios.put(
+             `https://cricketstore.onrender.com/api/v1/admin/product/${id}`,
+             productData,
+             config
+           );
 
-    const { data } = await axios.get(`https://cricketstore.onrender.com/api/v1/reviews?id=${productId}`, { withCredentials: true });
-    
-    dispatch({ type: ALL_REVIEW_SUCCESS, payload: data.reviews });
-  } catch (error) {
-    dispatch({ type: ALL_REVIEW_FAIL, payload: error.response?.data?.message || error.message });
-  }
-};
+           dispatch({
+             type: UPDATE_PRODUCT_SUCCESS,
+             payload: data.success,
+           });
+         } catch (error) {
+           dispatch({
+             type: UPDATE_PRODUCT_FAIL,
+             payload: error.message,
+           });
+         }
+       };
 
-// Delete review
-export const deleteProductReview = (reviewId, productId) => async (dispatch) => {
-  try {
-    dispatch({ type: DELETE_REVIEW_REQUEST });
+ // get all review of product admin ==>
+ export const getAllreviews  = (productId) => async (dispatch) =>{
 
-    const { data } = await axios.delete(
-      `https://cricketstore.onrender.com/api/v1/product/reviews/delete?id=${reviewId}&productId=${productId}`,
-      { withCredentials: true }
-    );
+     try {
+        dispatch({type : ALL_REVIEW_REQUEST})
 
-    dispatch({ type: DELETE_REVIEW_SUCCESS, payload: data.success });
-  } catch (error) {
-    dispatch({ type: DELETE_REVIEW_FAIL, payload: error.response?.data?.message || error.message });
-  }
-};
+const { data } = await axios.get(`https://cricketstore.onrender.com/api/v1/reviews?id=${productId}`, { withCredentials: true });        dispatch({type : ALL_REVIEW_SUCCESS , payload : data.reviews})
+     } catch (error) {
+        dispatch({type : ALL_REVIEW_FAIL , payload : error.message})
+     }
+ }
 
-// Clear Errors
-export const clearErrors = () => (dispatch) => {
+
+ // delete product review
+export const deleteProductReview = (reviewId , productId) => async (dispatch) =>{
+   try {
+  dispatch({type : DELETE_REVIEW_REQUEST})
+
+   const { data } = await axios.delete(
+  `https://cricketstore.onrender.com/api/v1/product/reviews/delete?id=${reviewId}&productId=${productId}`,
+  { withCredentials: true }
+);
+
+     dispatch({ type: DELETE_REVIEW_SUCCESS, payload: data.success });
+   } catch (error) {
+      dispatch({type : DELETE_REVIEW_FAIL , payload : error.message})
+   }
+
+}
+
+
+// clear error
+export const clearErrors = () => async (dispatch) => {
   dispatch({ type: CLEAR_ERRORS });
 };
